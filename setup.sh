2bfa636abe9c84917162b17b5f14d4219a0f4ed0
Qays H. Poonawala <qayshp@gmail.com>
2015-05-14 02:47:03 -0700

    Continue following Heroku directions.

--- setup.sh
+++ setup.sh
@@ -26,3 +26,38 @@ cat <<DELIM | sed 's/^    //' > Procfile
 DELIM
 
 pip freeze > requirements.txt
+
+cat <<DELIM | sed 's/^    //' >> hellodjango/settings.py
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+DELIM
+
+cat <<DELIM | sed 's/^    //' >> hellodjango/wsgi.py
+    from django.core.wsgi import get_wsgi_application
+    from dj_static import Cling
+
+    application = Cling(get_wsgi_application())
+DELIM
+
+cat <<DELIM | sed 's/^    //' >> ../.gitignore
+    venv
+    *.pyc
+    staticfiles
+DELIM
